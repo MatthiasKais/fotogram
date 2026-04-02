01244e3b3468a3091acecde07ff267d8e2b2994e
Matthias Kaiser <matthias.kaiser1@outlook.de>
2026-04-02 10:38:29 +0200

    added "script.js" with function to render imgs

---
+++ js/script.js
@@ -0,0 +1,49 @@
+
+
+let myImagesNames = [
+'brightfreak-stockholm-3897532.jpg',
+'detait-fruit-8773085.jpg',,
+'jackmac34-portugal-3561087.jpg',
+'jungr-death-valley-2314018.jpg',
+'loggawiggler-badwater-4546.jpg',
+'makalu-latvia-3725548.jpg',
+'manniguttenberger-monument-valley-5499156.jpg',
+'oljamu-yosemite-8177850.jpg',
+'qimono-buddha-5410319.jpg',
+'viarami-traffic-7033509.jpg',
+'walkerssk-yosemite-valley-2053308.jpg',
+'salofoto-dog-10187835.jpg'
+];
+
+
+let myImages = [
+    '/img/brightfreak-stockholm-3897532.jpg',
+    '/img/detait-fruit-8773085.jpg',
+    '/img/jackmac34-portugal-3561087.jpg',
+    '/img/jungr-death-valley-2314018.jpg',
+    '/img/loggawiggler-badwater-4546.jpg',
+    '/img/makalu-latvia-3725548.jpg',
+    '/img/manniguttenberger-monument-valley-5499156.jpg',
+    '/img/oljamu-yosemite-8177850.jpg',
+    '/img/qimono-buddha-5410319.jpg',
+    '/img/viarami-traffic-7033509.jpg',
+    '/img/walkerssk-yosemite-valley-2053308.jpg',
+    '/img/salofoto-dog-10187835.jpg',
+]
+
+
+function render() {
+    let contentRef = document.getElementById('content');
+    for (let index = 0; index < myImages.length; index++) {
+        contentRef.innerHTML += getNoteTemplate(index);
+    }
+}
+
+
+function getNoteTemplate(index){
+    return `    <img class="single-image" src="${myImages[index]}"> `
+}
+
+    // return `    <div class="single_element">
+    //                 <img src="${myImages[index]}">
+    //             </div>`
