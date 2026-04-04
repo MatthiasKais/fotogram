096b98fa77008413178f02725f002d26f18f64a3
Matthias Kaiser <matthias.kaiser1@outlook.de>
2026-04-04 07:27:35 +0200

    changed Dialog

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
 
 
-let myImagesNames = [
-'brightfreak-stockholm-3897532.jpg',
-'detait-fruit-8773085.jpg',,
-'jackmac34-portugal-3561087.jpg',
-'jungr-death-valley-2314018.jpg',
-'loggawiggler-badwater-4546.jpg',
-'makalu-latvia-3725548.jpg',
-'manniguttenberger-monument-valley-5499156.jpg',
-'oljamu-yosemite-8177850.jpg',
-'qimono-buddha-5410319.jpg',
-'viarami-traffic-7033509.jpg',
-'walkerssk-yosemite-valley-2053308.jpg',
-'salofoto-dog-10187835.jpg'
-];
+const openImgRef = document.getElementById("openImg");
 
+let myImagesNames = [
+    'brightfreak-stockholm-3897532.jpg',
+    'detait-fruit-8773085.jpg',
+   'jackmac34-portugal-3561087.jpg',
+    'jungr-death-valley-2314018.jpg',
+    'loggawiggler-badwater-4546.jpg',
+    'makalu-latvia-3725548.jpg',
+    'manniguttenberger-monument-valley-5499156.jpg',
+    'oljamu-yosemite-8177850.jpg',
+    'qimono-buddha-5410319.jpg',
+    'viarami-traffic-7033509.jpg',
+    'walkerssk-yosemite-valley-2053308.jpg',
+    'salofoto-dog-10187835.jpg'
+];
 
 let myImages = [
     './img/brightfreak-stockholm-3897532.jpg',
@@ -29,19 +30,31 @@ let myImages = [
     './img/viarami-traffic-7033509.jpg',
     './img/walkerssk-yosemite-valley-2053308.jpg',
     './img/salofoto-dog-10187835.jpg',
-]
+];
 
+function openDialog(index) {
+    console.log("Clicked image index:", index);
+    openImgRef.showModal();
+};
+
+function closeDialog(){
+    openImgRef.close();
+
+};
 
 function render() {
     let contentRef = document.getElementById('content');
     for (let index = 0; index < myImages.length; index++) {
-        contentRef.innerHTML += getNoteTemplate(index);
+         contentRef.innerHTML += getNoteTemplate(index);
     }
-}
+};
 
 function getNoteTemplate(index) {
-  return `<img class="single-image" onclick="openModal(${index})" src="${myImages[index]}" alt="${myImagesNames[index]}">`;
-}
+        return `<img class="single-image" onclick="openDialog(${index})" src="${myImages[index]}" alt="${myImagesNames[index]}">`;
+};
+
+
+
 
 
 
